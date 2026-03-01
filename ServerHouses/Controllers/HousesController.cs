07643e7ad1f96897de6970f23e7c22aad15c65e2
lifsc <lifsc@MSI>
2026-03-01 16:08:37 +0200

    Add project files.

---
+++ ServerHouses/Controllers/HousesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient; 
+using System.Data;             
+using System.Collections.Generic;
+using ServerHouses;
+using Microsoft.Extensions.Configuration;
+
+namespace ServerHouses.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HousesController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public HousesController(IConfiguration configuration)
+        {
+ 
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        [HttpPost("exec")]
+        public IActionResult Post([FromBody] MyJson request)
+        {
+
+            return Ok();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
